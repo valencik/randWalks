d6dd789b95c3b203d8ea3ab19ba272167134093c
Andrew Valencik <andrew.valencik@gmail.com>
2014-10-23 22:48:27 -0300

    Made pep8 compliant.

--- randWalk.py
+++ randWalk.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def saveFigure(name):
-  print("Saving pdf of " +name +"...")
-  plt.savefig(name+'.np'+str(nPaths)+'.ns'+str(nSteps)+'.pdf', bbox_inches='tight')
+    print("Saving pdf of " + name + "...")
+    plt.savefig(name + '.np' + str(nPaths) + '.ns' + str(nSteps) + '.pdf',
+                bbox_inches='tight')
 #  print("Saving " +name +" png")
 #  plt.savefig(name+'.png', bbox_inches='tight')
-  
+
 # Parameters
 nPaths = 400
 nSteps = 400
@@ -16,7 +18,7 @@ figureWidth = 24
 x = np.linspace(0, nSteps, nSteps)
 
 # Generate matrix of -1's and 1's, then take their cumulative sum
-y = 2* np.floor(2 * (np.random.random((nPaths, nSteps)) )) -1
+y = 2 * np.floor(2 * (np.random.random((nPaths, nSteps)))) - 1
 y = np.cumsum(y, axis=1)
 
 # Calculate the average distance from zero
@@ -24,7 +26,7 @@ avg = np.mean(np.absolute(y), axis=0)
 ysqrt = np.sqrt(2 * x / np.pi)
 
 # Initial figure
-fig, ax = plt.subplots(figsize=(figureWidth,figureWidth/2))
+fig, ax = plt.subplots(figsize=(figureWidth, figureWidth/2))
 
 # Plot!
 ax.plot(x, y.transpose(), color='blue', lw=0, alpha=0.0)
